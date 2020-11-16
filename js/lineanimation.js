a60aa243319ce49302ae3ace46910bc65dd26c46
Your Name <kge.819@gmail.com>
2020-11-15 17:46:19 -0800

    added the frame html for vis 1-3

---
+++ js/lineanimation.js
@@ -0,0 +1,20 @@
+//this is the second visualization and focused on global carbon emissions
+//I wasn't sure what to call it
+
+class LineAnimation {
+
+    constructor() {
+    }
+
+    initvis(){
+
+    }
+
+    wrangledata(){
+
+    }
+
+    updatevis(){
+
+    }
+}
