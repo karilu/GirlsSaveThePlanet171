f84e0beeeb9315c9fd61111a749af12897afb5f9
vicyingc <65056439+vicyingc@users.noreply.github.com>
2020-12-02 20:49:30 -1000

    Update bubblemap.js

--- js/bubblemap.js
+++ js/bubblemap.js
@@ -1,86 +1,124 @@
-class Bubblemap{
-    constructor(parentElement,data){
-        this.parentElement = parentElement
-        this.data = data
-        this.displayData = [];
-
-        let colors = ['#AA4839', '#AA7239', '#265B6A', '#2A7E43', '#582B73', '#882D63','#7EA136', '#ACAB39']
-
-        this.initvis()
-    }
-
-    initvis(){
-        let vis = this
-
-        vis.margin = {top: 40, right: 40, bottom: 40, left: 40};
+let margin = {top: 40, right: 40, bottom: 40, left: 40};
         //the dynamic sizing wasn't working so I hard coded it in the meantime
-        vis.width = $('#' + vis.parentElement).width() - vis.margin.left - vis.margin.right;
-        vis.height = $('#' + vis.parentElement).height() - vis.margin.top - vis.margin.bottom;
-        vis.innerRadius = 100;
-        vis.outerRadius = Math.min(vis.width, vis.height)/2
+let width = 660 - margin.left - margin.right;
+let height = 600 - margin.top - margin.bottom;
+let innerRadius = 100;
+let outerRadius = Math.min(width, height)/1.6
 
-        console.log(vis.data)
+// draw svg area
+let svg = d3.select('#bubbleMap').append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + width / 2 + "," + ( height/2+140 )+ ")");
 
+// Year parser
+let formatDate = d3.timeFormat("%Y");
+let parseDate = d3.timeParse("%Y");
+let x;
+let y;
+let selectValue;
+let data;
 
-        // svg drawing area
-        vis.svg = d3.select("#" + vis.parentElement).append("svg")
-            .attr("width", vis.width + vis.margin.left + vis.margin.right)
-            .attr("height", vis.height + vis.margin.top + vis.margin.bottom)
-            .append("g")
-            .attr("transform", "translate(" + vis.width / 2 + "," + ( vis.height/2+100 )+ ")");
+// Initialize data
+loadData();
 
-        // Xscale
-        vis.x = d3.scaleBand()
+// Load CSV file
+function loadData() {
+    d3.csv("data/energyconsumption.csv").then(function(csv) {
+
+        csv.forEach(function(d){
+            // Convert string to 'date object'
+            d.year = parseDate(d.year);
+
+            // Convert numeric values to 'numbers'
+
+            d.coal_consumption_EJ = +d.coal_consumption_EJ;
+            d.gas_consumption_EJ = +d.gas_consumption_EJ;
+            d.geo_biomass_other_TWh = +d.geo_biomass_other_TWh;
+            d.hydro_generation_TWh = +d.hydro_generation_TWh;
+            d.nuclear_generation_TWh = +d.nuclear_generation_TWh;
+            d.oil_consumption_EJ = +d.oil_consumption_EJ;
+            d.solar_generation_TWh = +d.solar_generation_TWh;
+            d.wind_generation_TWh = +d.wind_generation_TWh;
+        });
+
+        // Store csv data in global variable
+        data = csv;
+        console.log(data);
+
+        x = d3.scaleBand()
             .range([0, 2 * Math.PI])
             .align(0)
-            .domain(vis.data.map(function(d) {return d.Entity;}))
 
-        //Yscale
-        vis.y = d3.scaleRadial()
-            .range([vis.innerRadius, vis.outerRadius])
-            .domain([0, 10000]);
 
-        //Add bars
-        vis.svg.append("g")
-            .selectAll("path")
-            .data(vis.data)
-            .enter()
-            .append("path")
-                .attr("fill", "#69b3a2") // subject to change later
-                .attr("d", d3.arc()
-                    .innerRadius(vis.innerRadius)
-                    .outerRadius(function(d) { return vis.y(d['Value']); })// subject to change later
-                    .startAngle(function(d) { return vis.x(d.Entity); })
-                    .endAngle(function(d) { return vis.x(d.Entity) + vis.x.bandwidth(); })
-                    .padAngle(0.01)
-                    .padRadius(vis.innerRadius));
+        y = d3.scaleRadial()
+            .range([innerRadius, outerRadius])
 
-        //Add the labels
-        vis.svg.append("g")
-            .selectAll("g")
-            .data(vis.data)
-            .enter()
-            .append("g")
-            .attr("text-anchor", function(d) { return (vis.x(d.Entity) + vis.x.bandwidth() / 2 + Math.PI) % (2 * Math.PI) < Math.PI ? "end" : "start"; })
-            .attr("transform", function(d) { return "rotate(" + ((vis.x(d.Entity) + vis.x.bandwidth() / 2) * 180 / Math.PI - 90) + ")"+"translate(" + (vis.y(d['Value'])+10) + ",0)"; })
-            .append("text")
-            .text(function(d){return(d.Entity)})
-            .attr("transform", function(d) { return (vis.x(d.Entity) + vis.x.bandwidth() / 2 + Math.PI) % (2 * Math.PI) < Math.PI ? "rotate(180)" : "rotate(0)"; })
-            .style("font-size", "11px")
-            .attr("alignment-baseline", "middle")
+        
 
-        vis.wrangleData();
+        // Draw the visualization for the first time
+        updateVisualization();
+    });
+}
+
+
+function updateVisualization() {
+
+    selectValue = d3.select("#data-type").property("value");
+    console.log(selectValue);
+
+    // set year
+    const time = document.getElementById("year").value;
+
+    //return filtered data
+    let filteredData = data.filter(function (d) {
+        return formatDate(d.year) === time
+    });
+    // sort by continent
+    function Comparator(a, b) {
+        if (a.continent < b.continent) return 1;
+        if (a.continent < b.continent) return -1;
+        return 0;
     }
+    filteredData = filteredData.sort(Comparator);
+    // filteredData.continent.sort()
+    console.log(filteredData);
 
-    wrangleData(){
-        let vis = this;
+    // update scales
+    x.domain(filteredData.map(function(d) {return d.country;}));
+    y.domain([0, d3.max(filteredData, d =>d[selectValue])]);
 
+    let bubbleGraph = svg.append("g")
+        .selectAll("path")
+        .data(filteredData)
 
-        vis.updateVis();
-    }
+        bubbleGraph.enter()
+        .append("path")
+        .attr("fill", "#69b3a2")
+        .attr("d", d3.arc()     // imagine your doing a part of a donut plot
+            .innerRadius(innerRadius)
+            .outerRadius(function(d) { return y(d[selectValue]); })
+            .startAngle(function(d) { return x(d.country); })
+            .endAngle(function(d) { return x(d.country) + x.bandwidth(); })
+            .padAngle(0.01)
+            .padRadius(innerRadius))
 
-    updateVis(){
-        let vis = this;
+    let bubbleText = svg.append("g")
+        .selectAll("g")
+        .data(filteredData)
 
-    }
+        bubbleText.enter()
+        .append("g")
+        .attr("text-anchor", function(d) { return (x(d.country) + x.bandwidth() / 2 + Math.PI) % (2 * Math.PI) < Math.PI ? "end" : "start"; })
+        .attr("transform", function(d) { return "rotate(" + ((x(d.country) + x.bandwidth() / 2) * 180 / Math.PI - 90) + ")"+"translate(" + (y(d[selectValue])+10) + ",0)"; })
+        .append("text")
+        .text(function(d){return(d.country)})
+        .attr("transform", function(d) { return (x(d.country) + x.bandwidth() / 2 + Math.PI) % (2 * Math.PI) < Math.PI ? "rotate(180)" : "rotate(0)"; })
+        .style("font-size", "11px")
+        .attr("alignment-baseline", "middle")
+// NEED TO FIGURE OUT HOW TO MAKE THIS WORK
+    bubbleGraph.exit().remove();
+    bubbleText.exit().remove();
+
 }
